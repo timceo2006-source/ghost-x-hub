d43ef5ba2c2858be8893aae144c30d681c9be5a1
timceo2006-source <timceo2006@gmail.com>
2026-08-31 02:42:57 +0700

    Update UG.lua

--- UG.lua
+++ UG.lua
@@ -1,7 +1,7 @@
 local TARGET_PLACE_ID = 77649408247578
 
-local selectedMap = "Winter Outpost"
-local selectedDifficulty = "Hard"
+local selectedMap = "Pirate Island"
+local selectedDifficulty = "Insane"
 
 getgenv().AutoCreateAndStart = true
 getgenv().AutoFarmEnabled = true
