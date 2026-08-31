231d1342ca092c7db7c5b8af3b18ef62391262e3
timceo2006-source <timceo2006@gmail.com>
2026-08-31 11:29:50 +0700

    Update UG.lua

--- UG.lua
+++ UG.lua
@@ -217,7 +217,7 @@ function startFarm()
                     -- เช็คเพิ่มเติมว่ามี Head หรือลักษณะคล้ายมอนสเตอร์จริง
                     if obj:FindFirstChild("Head") or obj:FindFirstChild("HumanoidRootPart") then
                         currentTarget = obj
-                        hrp.Size = Vector3.new(25, 25, 25)
+                        hrp.Size = Vector3.new(25, 40, 25)
                         hrp.Transparency = 0.8
                         hrp.CanCollide = false
                         lastFoundMonsterTime = tick()
