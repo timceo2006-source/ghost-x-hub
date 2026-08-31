bbd628def6796d820bb923bbc10c4201de4542f3
timceo2006-source <timceo2006@gmail.com>
2026-08-31 16:43:23 +0700

    Update UG.lua

--- UG.lua
+++ UG.lua
@@ -350,7 +350,7 @@ function startFarm()
 
                         -- STEP 2: ขึ้นมารอ 1 วินาที ที่ความสูง 30
                         currentDynamicHeight = 30
-                        task.wait(1)
+                        task.wait(0.3)
 
                         -- STEP 3: ขึ้นไปรอคูลดาวน์ที่ความสูง 60
                         currentDynamicHeight = 60
