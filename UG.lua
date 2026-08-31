f1a8641f7fe492c9e33f5ed1b82d5e2321adba91
timceo2006-source <timceo2006@gmail.com>
2026-08-31 20:33:18 +0700

    Update UG.lua

--- UG.lua
+++ UG.lua
@@ -1,7 +1,7 @@
 local TARGET_PLACE_ID = 77649408247578
 
-local selectedMap = "King's Castle"
-local selectedDifficulty = "Nightmare"
+local selectedMap = "The Underworld"
+local selectedDifficulty = "Insane"
 
 -- ตั้งค่าฮิตบ็อกซ์
 local HITBOX_RADIUS = 150
