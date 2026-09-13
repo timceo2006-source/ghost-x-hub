e43059033281434c45b9bd361c35a0739b4067fd
timceo2006-source <timceo2006@gmail.com>
2026-09-13 23:52:10 +0700

    Update Animedice.lua

--- Animedice.lua
+++ Animedice.lua
@@ -1,4 +1,14 @@
-local Players = game:Getservice("Players")
+local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
+local myplot = nil
 
-
+for _, plot in pairs(workspace.Plots.Claimed:GetChildren()) do
+  if plot:FindFirstChild("Label") and plot.Label:FindFirstChild("BillboardGui") then
+    local PlayerUI = plot.Label.BillboardGui:FindFirstChild("PlayerName")
+    if PlayerUI.Text == LocalPlayer.Name or PlayerUI.Text == LocalPlayer.DisplayName then
+      myplot = plot
+      print("เจอละ")
+      break
+    end    
+  end
+end
