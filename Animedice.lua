8368b29857fdd30f920a734da23e4e6d5caa0e8a
timceo2006-source <timceo2006@gmail.com>
2026-09-18 23:56:10 +0700

    Update Animedice.lua

--- Animedice.lua
+++ Animedice.lua
@@ -1,5 +1,8 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local RS = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
 local SetAutoRoll = RS:WaitForChild("Network"):WaitForChild("RollService"):WaitForChild("RE"):WaitForChild("SetAutoRoll")
 
 local Window = WindUI:CreateWindow({
@@ -37,6 +40,16 @@ local AutoRollToggle = Tab:Toggle({
         if state then
             pcall(function()
                 SetAutoRoll:FireServer(true)
+                local rootUI = LocalPlayer.PlayerGui:FindFirstChild("Root")
+                if rootUI and rootUI:FindFirstChild("Rolling") then
+                    local hiddenBtn = rootUI.Rolling:FindFirstChild("Options") and rootUI.Rolling.Options:FindFirstChild("HiddenRoll")
+                    if hiddenBtn and getconnections then
+                        for _, connection in pairs(getconnections(hiddenBtn.MouseButton1Click)) do
+                            connection:Fire()
+                        end
+                    end
+                    rootUI.Rolling.Visible = false
+                end
             end)
         end
     end
